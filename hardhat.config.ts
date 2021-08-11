5de5ffc343067a603133ae06dd9c0126e897fff6
YutaSugimura <nextc3m@gmail.com>
2021-08-11 21:41:30 +0900

    cleanup: hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,11 @@
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
 import { config as dotEnvConfig } from 'dotenv';
 dotEnvConfig();
 
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
+task('accounts', 'Prints the list of accounts', async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
@@ -24,7 +23,7 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY ?? '';
  */
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.4",
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
